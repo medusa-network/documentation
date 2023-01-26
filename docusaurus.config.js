860955005a25df6b6e703af94a5bf36e65123749
Jonathan Easterman <jonathaneasterman@gmail.com>
2023-01-26 13:35:08 +0000

    update discord link to cryptonet discord

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
                     title: 'Community',
                     items: [{
                         label: 'Discord',
-                        href: 'https://discordapp.com/invite/docusaurus',
+                        href: 'https://discord.gg/cryptonet',
                     },],
                 },
                 {
